dd823fd16b64d7022f8c89f4c69cc083c7db5cc3
ashtonmcox <168942375+ashtonmcox@users.noreply.github.com>
2024-09-23 14:46:30 -0400

    Added functions to GoodCalc

---
+++ GoodCalc.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+
+    def add(self, value1, value2):
+        return value1 + value2
+
+    def subtract(self, value1, value2):
+        return value1 - value2
+
+    def multiply(self, value1, value2):
+        return value1 * value2
+
+    def divide(self, value1, value2):
+        return value1 / value2
+
