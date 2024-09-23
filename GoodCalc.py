26b2a0509ea92af633f7e2e0e43529b52103c7e6
ashtonmcox <168942375+ashtonmcox@users.noreply.github.com>
2024-09-23 15:10:53 -0400

    Added User input for GoodCalc

--- GoodCalc.py
+++ GoodCalc.py
@@ -15,3 +15,32 @@ class Calculator:
     def divide(self, value1, value2):
         return value1 / value2
 
+
+value1 = 0
+operation = 0
+value2 = 0
+value1 = float(input("Please enter your first number: "))
+value2 = float(input("Please enter your second number: "))
+
+calculator = Calculator(value1, value2)
+
+print("Please choose an operation from the options below")
+print("Operations")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+operation = int(input())
+
+if(operation == 1):
+    print(calculator.add(value1, value2))
+elif(operation == 2):
+    print(calculator.subtract(value1, value2))
+elif(operation == 3):
+    print(calculator.multiply(value1, value2))
+elif(operation == 4):
+    print(calculator.divide(value1, value2))
+else:
+    print("Please try again with a valid operation")
+
+
